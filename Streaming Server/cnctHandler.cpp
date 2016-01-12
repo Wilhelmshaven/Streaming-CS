e0021a124a4b1567852723bbf8abea1b87137232
Tintin <nntt19@live.com>
2016-01-12 20:29:23 +0800

    继续填入完成端口代码

--- Streaming Server/cnctHandler.cpp
+++ Streaming Server/cnctHandler.cpp
@@ -12,11 +12,29 @@ cnctHandler::cnctHandler()
 
 	completionPort = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 0, 0);   //建立完成端口
 
-}
+	//获取并填写服务器信息：监听任意地址
+	srvAddr.sin_addr.S_un.S_addr = htonl(INADDR_ANY);
+	srvAddr.sin_family = AF_INET;
+	srvAddr.sin_port = htons(atoi(srvPort.c_str()));
 
+	// 建立流式套接字，由于要使用重叠I/O，这里必须要使用WSASocket来初始化Socket
+	srvSocket = WSASocket(AF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);
+}
 cnctHandler::~cnctHandler()
 {
+	//清理工作
+	for (int i = 0; i < sysInfo.dwNumberOfProcessors * 2; i++)
+	{
+		PostQueuedCompletionStatus(completionPort, 0, 0, NULL);
+	}
+
+	CloseHandle(completionPort);
+	CloseHandle(acptThread);
+
+	shutdown(srvSocket, SD_BOTH);
 	closesocket(srvSocket);
+
+	WSACleanup();
 }
 
 //一系列返回信息的函数
@@ -25,10 +43,28 @@ cnctHandler* cnctHandler::getInstance()
 	return instance;
 }
 
+//设置服务器
+void cnctHandler::srvConfig(string port)
+{
+	srvPort = port;
+}
+
 //获取系统信息
 void cnctHandler::getSystemInfo()
 {
 	GetSystemInfo(&sysInfo);
+}
+
+//检查客户端是否活动
+bool cnctHandler::isSocketAlive()
+{
+	string msg = "isAlive?";
+	int byteSent;
+
+	byteSent = send(srvSocket, msg.c_str(), msg.size(), NULL);
+
+	if (byteSent == -1)return false;
+	else return true;
 }
 
 //建立工作者线程
@@ -45,8 +81,67 @@ void cnctHandler::buildThread()
 	}
 }
 
+//启动服务器
+int cnctHandler::startServer()
+{									 
+	listen(srvSocket, SOMAXCONN);        //将SOCKET设置为监听模式
+
+	CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)acptThread, NULL, NULL, NULL);
+
+	return 0;
+}
+
+//==========监听线程=============//
+UINT cnctHandler::acptThread()
+{
+	SOCKADDR_IN clientAddr;
+	int addrSize = sizeof(clientAddr);
+	PER_IO_OPERATION_DATA *PerIoData;    //单IO数据
+	PER_HANDLE_DATA PerHandleData;       //单句柄数据
+	SOCKET acptSocket = NULL;            //临时SOCKET
+
+	while (true)
+	{
+		// 接收连接，并分配完成端，下次考虑更换成AcceptEx()：accept、WSAAccept是同步操作，AcceptEx是异步操作
+		acptSocket = accept(srvSocket, (SOCKADDR*)&clientAddr, &addrSize);
+
+		//保存客户端信息
+		PerHandleData.clientAddr = clientAddr;
+		PerHandleData.clientSocket = acptSocket;
+
+		// 将接受套接字和完成端口关联
+		CreateIoCompletionPort((HANDLE)acptSocket, completionPort, (DWORD)acptSocket, 0);
+
+		// 开始在接受套接字上处理I/O，使用重叠I/O机制
+		PerIoData = (PER_IO_OPERATION_DATA *)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, sizeof(PER_IO_OPERATION_DATA));
+
+		// 在新建的套接字上投递一个或多个异步WSARecv或WSASend请求，这些I/O请求完成后，工作者线程会为I/O请求提供服务单I/O操作数据(I/O重叠)
+		ZeroMemory(PerIoData, sizeof(PerIoData));
+		PerIoData->databuff.len = BUF_SIZE;
+		PerIoData->databuff.buf = PerIoData->buffer;
+		PerIoData->operationType = 0;	// RECV_POSTED
+										
+		WSARecv(PerHandleData.clientSocket, &PerIoData->databuff, 1, &PerIoData->bytesRecv, &PerIoData->flags,
+			&(PerIoData->overlapped), NULL);
+	}
+}
+
 //==========工作者线程===========//
 UINT cnctHandler::workerThreadFunc()
 {
+	DWORD BytesTransferred;
+	SOCKET acptSocket = NULL;
+	PER_IO_OPERATION_DATA *PerIoData = NULL;
+	LPOVERLAPPED lpOverlapped = NULL;
+	char *FileName = new char[MAX_PATH];                       //Windows中最大文件名为260
+	char *buf = new char[BUF_SIZE];
+	BY_HANDLE_FILE_INFORMATION *FileInfo = new BY_HANDLE_FILE_INFORMATION;
+
+	while (true)
+	{
+		//得到完成端口的状态
+		GetQueuedCompletionStatus(completionPort, &BytesTransferred, (PULONG_PTR)&acptSocket, (LPOVERLAPPED *)&PerIoData, INFINITE);
+
+	}
 
 }
--- Streaming Server/cnctHandler.h
+++ Streaming Server/cnctHandler.h
@@ -1,6 +1,28 @@
 #pragma once在·
 #include "CommonHeaders.h"
 
+//重叠I/O需要用到的类，临时记录IO数据，这个并没有统一标准
+typedef struct PER_IO_OPERATION_DATA
+{
+	WSAOVERLAPPED overlapped;  // 每一个重叠I/O网络操作都要有一个
+	WSABUF databuff;           // 
+	char buffer[BUF_SIZE];     // 对应WSABUF里的缓冲区
+	DWORD bytesRecv;           // 接收到的数据量
+	DWORD flags;               //
+	int operationType;         // 标志这个重叠I/O操作是做什么的，例如Accept/Recv等
+};
+
+/**
+* 结构体名称：PER_HANDLE_DATA
+* 结构体存储：记录单个套接字的数据，包括了套接字的变量及套接字的对应的客户端的地址。
+* 结构体作用：当服务器连接上客户端时，信息存储到该结构体中，知道客户端的地址以便于回访。
+**/
+typedef struct PER_HANDLE_DATA
+{
+	SOCKET clientSocket;       // 这个I/O操作所使用的Socket
+	SOCKADDR_IN clientAddr;    // ...地址信息
+};
+
 class cnctHandler
 {
 public:
@@ -10,22 +32,23 @@ public:
 	static cnctHandler *getInstance();           //返回单例
 
 	//设置服务器参数
-	void srvConfig(int port);
+	void srvConfig(string port);
 
 	//启动服务器（直接进行到监听）
-	void startServer();
+	int startServer();
 
 	//线程相关
-	vector<HANDLE> workerThread;             //工作者线程，数量为CPU核数*2
-	static UINT workerThreadFunc();          //线程处理函数
+	vector<HANDLE> workerThread;               //工作者线程，数量为CPU核数*2
+	static UINT workerThreadFunc();            //线程处理函数
+	static UINT acptThread();                  //用于专门负责处理传入请求的线程
 
-	~cnctHandler();                   //必要的清理工作还是要写出来的
+	~cnctHandler();                            //必要的清理工作还是要写出来的
 
 private:
 	static cnctHandler *instance;              //单例
 	cnctHandler();                             //构造函数
 
-	SOCKET srvSocket;                          //唯一的SOCKET，避免乱改
+	static SOCKET srvSocket;                          //唯一的SOCKET，避免乱改
 
 	//禁止拷贝构造以及赋值
 	cnctHandler(const cnctHandler &);
@@ -47,12 +70,10 @@ private:
 	SYSTEM_INFO sysInfo;       //存储系统信息的结构	
 	SOCKADDR_IN srvAddr;       //服务器地址结构
 
-	HANDLE completionPort;     //完成端口
+	static HANDLE completionPort;     //完成端口
 
 	//Function
 	void getSystemInfo();          //获取系统信息（最主要是CPU核数，以便创建Worker线程）
 	void buildThread();            //根据CPU核数建立工作者线程
-	bool isSocketAlive(SOCKET s);
-
-	
+	bool isSocketAlive();
 };
