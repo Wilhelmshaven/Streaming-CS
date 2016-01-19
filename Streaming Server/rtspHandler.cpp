a6bec8783ff5ab9529d5d0ae7aa1140456a8fe6b
Tintin <nntt19@live.com>
2016-01-19 16:27:44 +0800

    增加NTP时间获取与简单SDP编码功能，继续推进服务器信令解析

--- Streaming Server/rtspHandler.cpp
+++ Streaming Server/rtspHandler.cpp
@@ -60,6 +60,126 @@ string rtspErrHandler::getErrMsg(int code)
 	return msg;
 }
 
+/*----------------------NTP时间戳获取类---------------------*/
+//获取NTP时间（距1900.1.1 00:00:00）
+unsigned long NTPTime::getNTPTime()
+{
+	SYSTEMTIME sysTime;
+	GetSystemTime(&sysTime);
+
+	unsigned long time = 0;
+
+	long tmp[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+
+	time += (sysTime.wYear - 1900) * 365;
+	for (int i = 1900; i < sysTime.wYear; ++i)
+	{
+		if (i % 4 == 0 && i != 1900)++time;
+	}
+
+	for (int i = 1; i < sysTime.wMonth; ++i)time += tmp[i];
+
+	time += sysTime.wDay - 1;
+
+	time *= 86400;
+	time += (sysTime.wHour - 1) * 3600;
+	time += (sysTime.wMinute - 1) * 60;
+	time += sysTime.wSecond;
+
+	return time;
+}
+
+//获取NTP时间（Unix版本）（距1970.1.1 00:00:00）
+unsigned long NTPTime::getNTPTimeUnix()
+{
+	unsigned long time = 0;
+
+	const unsigned long seventyYear = 2208988800;
+
+	time = getNTPTime() - seventyYear;
+
+	return time;
+}
+
+/*----------------------SDP编码器---------------------*/
+sdpEncoder::sdpEncoder()
+{
+	proVer = "v=0\r\n";
+}
+
+string sdpEncoder::encodeMsg()
+{
+	//  Session description
+	//	v = (protocol version)
+	//	o = (owner / creator and session identifier).
+	//	s = (session name)
+	//	i = *(session information)
+	//	u = *(URI of description)
+	//	e = *(email address)
+	//	p = *(phone number)
+	//	c = *(connection information - not required if included in all media)
+	//	b = *(bandwidth information)
+	//	One or more time descriptions(see below)
+	//	z = *(time zone adjustments)
+	//	k = *(encryption key)
+	//	a = *(zero or more session attribute lines)
+	//	Zero or more media descriptions(see below)
+
+	//	Time description
+	//	t = (time the session is active)
+	//	r = *(zero or more repeat times)
+
+	//	Media description
+	//	m = (media name and transport address)
+	//	i = *(media title)
+	//	c = *(connection information - optional if included at session - level)
+	//	b = *(bandwidth information)
+	//	k = *(encryption key)
+	//	a = *(zero or more media attribute lines)
+
+	//An example SDP description is :
+
+	//v = 0
+	//o = mhandley 2890844526 2890842807 IN IP4 126.16.64.4
+	//s = SDP Seminar
+	//i = A Seminar on the session description protocol
+	//u = http://www.cs.ucl.ac.uk/staff/M.Handley/sdp.03.ps
+	//e = mjh@isi.edu(Mark Handley)
+	//c = IN IP4 224.2.17.12 / 127
+	//t = 2873397496 2873404696
+	//a = recvonly
+	//m = audio 49170 RTP / AVP 0
+	//m = video 51372 RTP / AVP 31
+	//m = application 32416 udp wb
+	//a = orient:portrait
+	string tmp;
+
+	owner = "o=guest " + to_string(ntpTime.getNTPTimeUnix()) + " " + to_string(ntpTime.getNTPTimeUnix()) + " " + "IN IP4 www.rayion.com\r\n";
+
+	sessionName = "s=streaming Test\r\n";
+
+	timeActive = "t=" + to_string(ntpTime.getNTPTimeUnix()) + " 0\r\n";
+
+	sdpMsg = proVer + owner + sessionName + timeActive;
+
+	return sdpMsg;
+}
+
+/*----------------------随机会话号生成器---------------------*/
+sessionGenerator::sessionGenerator()
+{
+	session.resize(9);
+}
+//这里直接使用时间戳来作为会话号
+string sessionGenerator::getSessionID()
+{
+	unsigned long time = ntpTime.getNTPTimeUnix();
+
+	sprintf_s((char *)session.data(), 9, "%X", time);
+
+	return session;
+}
+
 /*----------------------RTSP连接处理器---------------------*/
 //直接初始化
 rtspHandler *rtspHandler::instance = new rtspHandler();
@@ -138,6 +258,8 @@ string rtspHandler::msgCodec(string msg)
 	rtspVersion = tmp.substr(tmp.find(' '));
 	URI = tmp.substr(0, tmp.find(' '));
 
+	//Todo:检查URI是否合法，不合法则返回相应错误信息
+
 	//3. Get sequence number
 	//直接复制整行就行，并不关心具体是多少，原样转回即可
 	//注意要留下\r\n（当然也可以要么就都不留，改成find \r，而不是find \n + 1)\）
@@ -156,7 +278,8 @@ string rtspHandler::msgCodec(string msg)
 		}
 	}
 
-	//---------------根据指令做出个性解析与答复----------------//
+	//---------------根据指令做出针对性解析与答复----------------//
+
 	paddingMsg = msg.find(msg.find(seqNum) + seqNum.length());
 	int errCode;   //回令的错误码
 	errCode = 200; //初始化为200：OK
@@ -175,9 +298,9 @@ string rtspHandler::msgCodec(string msg)
 		/* 
 		C->S
 		OPTIONS * RTSP/1.0
-        CSeq: 1
-        Require: implicit-play
-        Proxy-Require: gzipped-messages
+		CSeq: 1
+		Require: implicit-play
+		Proxy-Require: gzipped-messages
 
 		S->C
 		RTSP/1.0 200 OK
@@ -212,29 +335,29 @@ string rtspHandler::msgCodec(string msg)
 		/*
 		C->S
 		DESCRIBE rtsp://server.example.com/fizzle/foo RTSP/1.0
-        CSeq: 312
-        Accept: application/sdp, application/rtsl, application/mheg
+		CSeq: 312
+		Accept: application/sdp, application/rtsl, application/mheg
 
 		S->C
 		RTSP/1.0 200 OK
-        CSeq: 312
-        Date: 23 Jan 1997 15:35:06 GMT
-        Content-Type: application/sdp
-        Content-Length: 376
+		CSeq: 312
+		Date: 23 Jan 1997 15:35:06 GMT
+		Content-Type: application/sdp
+		Content-Length: 376
 
-        v=0
-        o=mhandley 2890844526 2890842807 IN IP4 126.16.64.4
-        s=SDP Seminar
-        i=A Seminar on the session description protocol
-        u=http://www.cs.ucl.ac.uk/staff/M.Handley/sdp.03.ps
-        e=mjh@isi.edu (Mark Handley)
-        c=IN IP4 224.2.17.12/127
-        t=2873397496 2873404696
-        a=recvonly
-        m=audio 3456 RTP/AVP 0
-        m=video 2232 RTP/AVP 31
-        m=whiteboard 32416 UDP WB
-        a=orient:portrait
+		v=0
+		o=mhandley 2890844526 2890842807 IN IP4 126.16.64.4
+		s=SDP Seminar
+		i=A Seminar on the session description protocol
+		u=http://www.cs.ucl.ac.uk/staff/M.Handley/sdp.03.ps
+		e=mjh@isi.edu (Mark Handley)
+		c=IN IP4 224.2.17.12/127
+		t=2873397496 2873404696
+		a=recvonly
+		m=audio 3456 RTP/AVP 0
+		m=video 2232 RTP/AVP 31
+		m=whiteboard 32416 UDP WB
+		a=orient:portrait
 		*/
 
 		//首先检查资源是否存在，这块写一个函数
@@ -246,9 +369,47 @@ string rtspHandler::msgCodec(string msg)
 		cmdLine += to_string(errCode) + " " + errHandler.getErrMsg(errCode) + "\r\n";
 		response = cmdLine + seqNum;
 
-		//日期，类型，长度
+		if (errCode != 200)break;
+
+		//下面是正常的信息内容
+
+		//日期，类型，长度  Date: 23 Jan 1997 15:35:06 GMT
+		SYSTEMTIME sysTime;
+		GetSystemTime(&sysTime);
+
+		tmp.resize(BUF_SIZE);
+		sprintf_s((char *)tmp.data(), BUF_SIZE, "Date: %02d %s %s %02d:%02d:%02d GMT\r\n",
+			sysTime.wDay, sysTime.wMonth, sysTime.wYear, sysTime.wHour, sysTime.wMinute, sysTime.wSecond);
+		tmp = tmp.substr(0, tmp.rfind('\n'));
+		tmp += "Content-Type: application/sdp\r\nContent-Length: ";
+
+		response += tmp;
 
 		//下面是SDP信息
+		tmp = sdpHandler.encodeMsg();
+		response = response + to_string(tmp.length()) + "\r\n\r\n";
+
+		response += tmp;
+
+		break;
+	}
+	case SETUP:
+	{
+		/*Example Message*/
+		/*
+		C->S: SETUP rtsp ://example.com/foo/bar/baz.rm RTSP/1.0
+		CSeq : 302
+		Transport : RTP / AVP; unicast; client_port = 4588 - 4589
+
+		S->C: RTSP / 1.0 200 OK
+		CSeq : 302
+		Date : 23 Jan 1997 15 : 35 : 06 GMT
+		Session : 47112344
+		Transport : RTP / AVP; unicast;
+		client_port = 4588 - 4589; server_port = 6256 - 6257
+		*/
+
+		//首先解析客户端请求的传送方式和端口号，然后回发会话号
 
 		break;
 	}
--- Streaming Server/rtspHandler.h
+++ Streaming Server/rtspHandler.h
@@ -16,9 +16,72 @@ private:
 	map<int, string> errCodeList;    //错误链接表
 };
 
+//NTP时间获取类
+class NTPTime
+{
+public:
+	unsigned long getNTPTime();
+	unsigned long getNTPTimeUnix();
+};
+
+//SDP编码器
+class sdpEncoder
+{
+public:
+	string encodeMsg();
+
+	sdpEncoder();
+
+private:
+	NTPTime ntpTime;
+	string sdpMsg;
+
+	//--Session description
+	//Require
+	string proVer;
+	string owner;
+	string sessionName;
+	//Option
+	string sessionInfo;
+	string URI;
+	string emailAddr;
+	string phoneNum;
+	string cnctInfo;
+	string bandwidthInfo;
+	string timeZoneAdjust;
+	string encryptKey;
+	string moreSessionAttr;
+
+	//Time description
+	string timeActive;     //require
+	string repeatTime;     //option
+
+	//Media description
+	string mediaNameAndAddr;     //require
+	string mediaTitle;
+	//string cnctInfo;             //optional if included at session-level
+	//string bandwidthInfo;
+	//string encryptKey;
+	//string moreSessionAttr;
+};
+
+//一个随机Session号生成器,气氛上我觉得直接用时间戳就好了……算了一下FFFFFFFF是足够的
+class sessionGenerator
+{
+private:
+	string session;
+	
+	NTPTime ntpTime;
+
+public:
+	sessionGenerator();
+
+	string getSessionID();
+};
+
 //RTSP消息的处理
 //编码依赖解码结果，这与客户端有着比较大的区别
-//每个客户端的请求都不一样，所以要编解码一体
+//每个客户端的请求都不一样，所以要编解码一体（先这样吧，虽然我觉得还是可以拆开的）
 class rtspHandler
 {
 public:
@@ -38,9 +101,10 @@ private:
 	string rtspVersion;                        //RTSP版本
 
 	rtspErrHandler errHandler;                 //错误信息处理器
+	sdpEncoder sdpHandler;                     //SDP编码器
 
 	vector<string> rtspMethod;                 //服务器rtsp方法
-	vector<bool> availableMethod;               //可用的服务器rtsp方法
+	vector<bool> availableMethod;              //可用的服务器rtsp方法
 
 	//禁止拷贝构造以及赋值
 	rtspHandler(const rtspHandler &);
