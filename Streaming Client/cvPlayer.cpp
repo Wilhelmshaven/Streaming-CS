7789179a7072d7313939d287413da1bd36ce2879
Wilhelmshaven <nntt19@163.com>
2016-02-27 01:38:38 +0800

    完成客户端播放器模拟模块（差中间件部分）

--- Streaming Client/cvPlayer.cpp
+++ Streaming Client/cvPlayer.cpp
@@ -2,48 +2,132 @@
 
 #include "cvPlayer.h"
 
-/*
-	全局变量
-*/
-//OpenCV矩阵（帧）
-Mat frame;
+//类内静态成员变量定义
+HANDLE cvPlayer::hEventStart;
+HANDLE cvPlayer::hEventShutdown;
+HANDLE cvPlayer::hEventDestroy;
+int cvPlayer::frameRate;
 
-
-cvPlayer *cvPlayer::instance = new cvPlayer;
+cvPlayer* cvPlayer::instance = new cvPlayer;
 
 cvPlayer * cvPlayer::getInstance()
 {
 	return instance;
 }
 
-int cvPlayer::buildPlayer()
+void cvPlayer::play()
 {
-	return 0;
+	SetEvent(hEventStart);
+	ResetEvent(hEventShutdown);
 }
 
-int cvPlayer::playBuffer(vector<int> *img)
+void cvPlayer::setFrameRate(int rate)
 {
-	Mat tmpFrame;
+	frameRate = 1000 / rate;
+}
 
+void cvPlayer::shutdown()
+{
+	SetEvent(hEventShutdown);
+	ResetEvent(hEventStart);
+}
 
-
-	return 0;
+void cvPlayer::destroyPlayer()
+{
+	SetEvent(hEventDestroy);
+	SetEvent(hEventShutdown);
+	SetEvent(hEventStart);
 }
 
 cvPlayer::~cvPlayer()
 {
+
 }
 
-int cvPlayer::vecToMat(vector<int>& img)
+DWORD cvPlayer::playThreadFunc(LPVOID lparam)
 {
-	vector<int>::iterator iter, end;
-	end = img.end();
-
-	for (iter = img.begin(); iter < end; ++iter)
+	while (1)
 	{
-		frame.push_back(iter);
+		WaitForSingleObject(hEventStart, INFINITE);
+
+		//获取图像缓存单例
+		imgBuffer *imgBuf = imgBuffer::getInstance();
+
+		/*
+			创建一个空白图像用来显示
+			首先获取屏幕大小，随后创建
+
+			CV_[The number of bits per item][Signed or Unsigned][Type Prefix]C[The channel number]
+			CV_8UC3：0-255，无符号，3通道
+
+			建立出来的图像，每个像素都是（205,205,205）的灰色
+		*/
+		Mat blankImg;
+
+		int nScreenWidth, nScreenHeight;
+
+		nScreenWidth = GetSystemMetrics(SM_CXSCREEN);
+		nScreenHeight = GetSystemMetrics(SM_CYSCREEN);
+
+		blankImg.create(nScreenHeight, nScreenWidth, CV_8UC3);
+
+		string windowName = "Streaming Client Media Player";
+		namedWindow(windowName, CV_WINDOW_AUTOSIZE);
+
+		imshow(windowName, blankImg);
+
+		/*
+			读取缓存->播放，循环
+		*/
+
+		Mat frame;
+
+		while (1)
+		{
+			//成功获取到图像则继续，否则检查退出事件
+			if (imgBuf->popBuffer(frame))
+			{
+				imshow(windowName, frame);
+			}
+
+			if (WaitForSingleObject(hEventShutdown, 0) == WAIT_OBJECT_0)
+			{
+				break;
+			}
+
+			//控制帧率，并等待可能的输入
+			int key = waitKey(frameRate);
+			if (key != -1)
+			{
+				//有输入，转发指令
+				//TODO：播放器和指令模块之间需要一个中间件
+				//按键队列，我这里入队，那边检测到有之后出队
+
+
+
+
+
+			}
+		}
+
+		if (WaitForSingleObject(hEventDestroy, 0) == WAIT_OBJECT_0)break;
 	}
 	
-
 	return 0;
 }
+
+/*
+	构造函数
+
+	主要任务：
+	初始化事件，创建线程
+*/
+cvPlayer::cvPlayer()
+{
+	hEventStart = CreateEvent(NULL, TRUE, FALSE, NULL);
+	hEventShutdown = CreateEvent(NULL, TRUE, FALSE, NULL);
+	hEventDestroy = CreateEvent(NULL, TRUE, FALSE, NULL);
+
+	CreateThread(NULL, NULL, playThreadFunc, NULL, NULL, NULL);
+}
+
--- Streaming Client/cvPlayer.h
+++ Streaming Client/cvPlayer.h
@@ -5,14 +5,14 @@
 
 	使用OpenCV的原因：比较熟悉，编写快捷，也能够轻易获取键盘输入，且该部分本身不属于核心内容
 
-	设计：
-	一个转换模块，把vector<int>结构传来的数据转为矩阵
-	一个图像队列，存储到来的图像
-	一个播放器，从队列中取出图像并播放，同时获取键盘消息
+	设计：一个播放器，从队列中取出图像并播放，同时获取键盘消息
 */
 
 #pragma once
 #include "CommonHeaders.h"
+
+//载入图像缓存模块
+#include "imageQueue.h"
 
 #include "opencv2\highgui\highgui.hpp"
 
@@ -23,35 +23,57 @@ using namespace cv;
 
 	使用：
 
+	void play()：启动播放器
+
+	static void setFrameRate(int frameRate = 20)：指定帧率，单位为帧数每秒
+
+	void shutdown()：关闭播放器（但仍可以重启）
+
+	void destroyPlayer()：摧毁播放器，顾名思义，调用以后你再也不能重启播放器了，只能重启客户端
 */
 class cvPlayer
 {
 public:
 	static cvPlayer* getInstance();
 
-	int buildPlayer();
+	void play();
 
-	int playBuffer(vector<int> *img);
+	static void setFrameRate(int frameRate = 20);
 
-	
+	void shutdown();
 
-
+	void destroyPlayer();
 
 	~cvPlayer();
 
 private:
 
-	int vecToMat(vector<int> &img);
+	//帧率（这里的意义是两帧之间间隔的毫秒数，即帧率的倒数）
+	static int frameRate;
 
+	/*
+		线程相关：播放器肯定是要线程的嘛……
+	*/
 
-	//OpenCV的Mat格式图像队列 
-	queue<Mat> imgQueue;
+	//开始事件
+	static HANDLE hEventStart;
+
+	//结束事件（但仍可再开始）
+	static HANDLE hEventShutdown;
+
+	//彻底结束事件（导致退出线程）
+	static HANDLE hEventDestroy;
+
+	//播放器线程
+	static DWORD WINAPI playThreadFunc(LPVOID lparam);
 
 	/*
 		单例模式相关
 	*/
-	static cvPlayer *instance;
+
 	cvPlayer();
+
+	static cvPlayer *instance;
 
 	cvPlayer(const cvPlayer &);
 	cvPlayer &operator=(const cvPlayer &);
